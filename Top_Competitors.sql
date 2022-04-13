b0825274f7b10b510b9985f7db10072ff8a83979
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2022-04-14 02:14:45 +0900

    Create Top_Competitors.sql

---
+++ Top_Competitors.sql
@@ -0,0 +1,9 @@
+SELECT S.hacker_id, H.name
+FROM Submissions S
+JOIN Challenges C ON S.challenge_id = C.challenge_id
+JOIN Difficulty D ON C.difficulty_level = D.difficulty_level
+JOIN Hackers H ON S.hacker_id = H.hacker_id
+WHERE S.score = D.score
+GROUP BY H.hacker_id, H.name
+HAVING COUNT(S.challenge_id) >= 2
+ORDER BY COUNT(S.challenge_id) DESC, S.hacker_id
