399c9f16f1726253d851fde147ea5396da212e24
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2022-04-14 14:31:38 +0900

    Create Ollivander’s_Inventory.sql

---
+++ Ollivander’s_Inventory.sql
@@ -0,0 +1,10 @@
+--https://www.hackerrank.com/challenges/harry-potter-and-wands/problem?isFullScreen=true
+
+SELECT W.id, P.age, W.coins_needed, W.power
+FROM (SELECT code, MIN(coins_needed) as coins_needed, power
+      FROM Wands
+      GROUP BY code, power) as M
+JOIN Wands W ON M.code = W.code AND M.coins_needed = W.coins_needed AND M.power = W.power
+JOIN Wands_Property P ON W.code = P.code
+WHERE P.is_evil = 0
+ORDER BY W.power DESC, P.age DESC
